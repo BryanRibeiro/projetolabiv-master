8b53c006f287107ebfd52e1ffe6a8c73e6dd8e54
Bryan Ribeiro <bryan.ribeiro@fatec.sp.gov.br>
2022-08-23 23:58:40 +0000

    mapeamento da tabela usuario

---
+++ src/main/java/br/gov/sp/fatec/projetolabiv/entity/Usuario.java
@@ -0,0 +1,49 @@
+package br.gov.sp.fatec.projetolabiv.entity;
+
+import javax.persistence.Column;
+import javax.persistence.ColumnResult;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "usr_usuario")
+public class Usuario {
+ 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "usr_id")
+    private Long id;
+
+    @Column(name = "usr_nome")
+    private String name;
+
+    @Column(name = "usr_senha")
+    private String senha;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}

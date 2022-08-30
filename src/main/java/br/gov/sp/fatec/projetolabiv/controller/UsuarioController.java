5694b82e92156696e99820d0d26c4ee39f61ea4f
Bryan Ribeiro <bryan.ribeiro@fatec.sp.gov.br>
2022-08-30 23:44:46 +0000

    usuariocontroller

---
+++ src/main/java/br/gov/sp/fatec/projetolabiv/controller/UsuarioController.java
@@ -0,0 +1,28 @@
+package br.gov.sp.fatec.projetolabiv.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.gov.sp.fatec.projetolabiv.entity.Usuario;
+import br.gov.sp.fatec.projetolabiv.service.SegurancaService;
+
+@RestController
+@CrossOrigin
+@RequestMapping(value = "/usuario")
+public class UsuarioController {
+    
+    @Autowired
+    private SegurancaService segurancaService;
+
+    @GetMapping
+    public List<Usuario> todosUsuarios() {
+        return segurancaService.listarTodosUsuarios();
+    }
+
+
+}
